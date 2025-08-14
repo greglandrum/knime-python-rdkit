8d020b011a586a6795e9eb2843a94547b8fca271
Greg Landrum <greg.landrum@gmail.com>
2025-08-14 18:18:44 +0200

    test all formats

--- src/test_standardizer_parent.py
+++ src/test_standardizer_parent.py
@@ -13,32 +13,47 @@ from . import standardizer_parent
 import knime.extension as knext
 import knime.extension.testing as ktest
 
+def run_if_not_none(func,mol):
+    if mol is not None:
+        return func(mol)
+    return None
+
 class TestCase(unittest.TestCase):
-  def testFragmentParent(self):
-    to_run = {
-      'Largest fragment': rdMolStandardize.FragmentParent,
-      'Remove charge': rdMolStandardize.ChargeParent,
-    }
-    inputDF = pd.DataFrame({'names':['mol1','mol2','mol3'], 'smiles': ['C.c1ccccc1', 'c1ccccc1C(=O)[O-].[Na+]', 'C1CCCCC1']})
-    for nm, func in to_run.items():
-      refDf = inputDF.copy()
-      refDf['Parent Molecule'] = [func(Chem.MolFromSmiles(smi)) for smi in inputDF['smiles']]
+  smis = ['C.c1ccccc1', 'c1ccccc1C(=O)[O-].[Na+]',
+    'C1CCCCC1', '[13CH3]CO', 'C[C@H](F)Cl', 'C[C@@H](F)Cl',
+    'Cc1[nH]ncc1', 'Cc1n[nH]cc1','bad']
+  mols = [Chem.MolFromSmiles(smi) for smi in smis]
+  sdfs = [run_if_not_none(Chem.MolToMolBlock, mol) for mol in mols]
+  nms = [f'mol{i+1}' for i in range(len(smis))]
 
-      # create and configure node
-      node = standardizer_parent.GetParentMoleculeNode()
-      node.molecule_column_param = 'smiles'
-      node.stand_action_param = nm
 
-      # "execute" node
-      exec_context = ktest.TestingExecutionContext()
-      tbl = knext.Table.from_pandas(inputDF)
-      result = node.execute(exec_context, tbl, force_molecule_type='smiles')
-      resultDf = result.to_pandas()
+  def test_get_parents_smiles(self):
+    to_run = standardizer_parent.GetParentMoleculeNode().standardization_actions
+    self.assertEqual(len(to_run), 6)
+    inputDF = pd.DataFrame({'names':self.nms, 'smiles':self.smis, 'sdf':self.sdfs,
+          'rdkit':self.mols})
+    for fmt in ['smiles','sdf','rdkit']:
+      for nm, func in to_run.items():
+        refDf = inputDF.copy()
+        refDf['Parent Molecule'] = [run_if_not_none(func, m) for m in inputDF['rdkit']]
 
-      # check results
-      expectedSmiles = [Chem.MolToSmiles(mol) for mol in refDf['Parent Molecule']]
-      resultSmiles = [Chem.MolToSmiles(mol) for mol in resultDf['Parent Molecule']]
-      self.assertEqual(resultSmiles, expectedSmiles)
+        # create and configure node
+        node = standardizer_parent.GetParentMoleculeNode()
+        node.molecule_column_param = fmt
+        node.stand_action_param = nm
+
+        # "execute" node
+        exec_context = ktest.TestingExecutionContext()
+        tbl = knext.Table.from_pandas(inputDF)
+        result = node.execute(exec_context, tbl, force_molecule_type=fmt)
+        resultDf = result.to_pandas()
+
+        # check results
+        expectedSmiles = [run_if_not_none(Chem.MolToSmiles, mol) for mol in refDf['Parent Molecule']]
+        resultSmiles = [run_if_not_none(Chem.MolToSmiles, mol) for mol in resultDf['Parent Molecule']]
+        self.assertEqual(resultSmiles, expectedSmiles)
+        origSmiles = [run_if_not_none(Chem.MolToSmiles, mol) for mol in self.mols]
+        self.assertNotEqual(resultSmiles, origSmiles)
 
 
 
